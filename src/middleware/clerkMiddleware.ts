2c749a0bbfc64bc4e94f091ab4a24c5f832598e7
Priyanshu-Senslyze <priyanshu.suman@senslyze.com>
2024-10-31 19:45:09 +0530

    Build script added

--- src/middleware/clerkMiddleware.ts
+++ src/middleware/clerkMiddleware.ts
@@ -1,23 +1,23 @@
-import { NextFunction, Request, Response } from "express";
-import { Clerk } from "@clerk/clerk-sdk-node";
-import dotenv from "dotenv";
+// import { NextFunction, Request, Response } from "express";
+// import { Clerk } from "@clerk/clerk-sdk-node";
+// import dotenv from "dotenv";
 
-dotenv.config();
+// dotenv.config();
 
-const clerk = new Clerk({ apiKey: process.env.CLERK_API_KEY as string });
+// const clerk = new Clerk({ apiKey: process.env.CLERK_API_KEY as string });
 
-export const clerkMiddleware = async (req: Request, res: Response, next: NextFunction) => {
-    const { authorization } = req.headers;
+// export const clerkMiddleware = async (req: Request, res: Response, next: NextFunction) => {
+//     const { authorization } = req.headers;
 
-    if (!authorization) {
-        return res.status(401).json({ error: "Authorization token missing" });
-    }
+//     if (!authorization) {
+//         return res.status(401).json({ error: "Authorization token missing" });
+//     }
 
-    try {
-        const token = authorization.split(" ")[1];
-        await clerk.verifySession(token);
-        next();
-    } catch (error) {
-        return res.status(401).json({ error: "Invalid authorization token" });
-    }
-};
+//     try {
+//         const token = authorization.split(" ")[1];
+//         await clerk.verifySession(token);
+//         next();
+//     } catch (error) {
+//         return res.status(401).json({ error: "Invalid authorization token" });
+//     }
+// };
