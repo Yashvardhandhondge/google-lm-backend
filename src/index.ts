d16643a1c00dc12d2c4d916038414f5324d0cdc4
Priyanshu-Senslyze <priyanshu.suman@senslyze.com>
2024-11-07 02:25:11 +0530

    Routing changed as per vercel

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,6 @@ import cors from "cors";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
 const API_URL = process.env.API_URL;
 
 // Middleware
@@ -31,6 +30,5 @@ app.get("/", (req, res) => {
     res.send("Hello, World!");
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+// Export the app for Vercel
+export default app;
